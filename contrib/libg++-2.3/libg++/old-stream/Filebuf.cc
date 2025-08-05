7607bd5ccccee9094e08a304f6f8d78ae1983da8
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-08-05 16:19:14 -0700

    Simplify Makefiles for 4.4BSD build support
    Replaced complex and legacy build logic in Emacs and GCC Makefiles with simplified rules targeting 4.4BSD. The new Makefiles now focus on setting up architecture-specific symlinks and invoking make, removing extensive configuration, build, and install targets that are no longer needed.

--- contrib/libg++-2.3/libg++/old-stream/Filebuf.cc
+++ contrib/libg++-2.3/libg++/old-stream/Filebuf.cc
@@ -2,325 +2,136 @@
 Copyright (C) 1990 Free Software Foundation
     written by Doug Lea (dl@rocky.oswego.edu)
 
-This file is part of the GNU C++ Library.  This library is free
-software; you can redistribute it and/or modify it under the terms of
-the GNU Library General Public License as published by the Free
-Software Foundation; either version 2 of the License, or (at your
-option) any later version.  This library is distributed in the hope
-that it will be useful, but WITHOUT ANY WARRANTY; without even the
-implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the GNU Library General Public License for more details.
-You should have received a copy of the GNU Library General Public
-License along with this library; if not, write to the Free Software
-Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+This file is part of GNU CC.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY.  No author or distributor
+accepts responsibility to anyone for the consequences of using it
+or for whether it serves any particular purpose or works at all,
+unless he says so in writing.  Refer to the GNU CC General Public
+License for full details.
+
+Everyone is granted permission to copy, modify and redistribute
+GNU CC, but only under the conditions described in the
+GNU CC General Public License.   A copy of this license is
+supposed to have been given to you along with GNU CC so you
+can know your rights and responsibilities.  It should be in a
+file named COPYING.  Among other things, the copyright notice
+and this notice must be preserved on all copies.  
 */
 
+#if 1
 #ifdef __GNUG__
 #pragma implementation
 #endif
-
-#include <Filebuf.h>
+#endif
 
 #include <std.h>
 #include <sys/file.h>           // needed to determine values of O_RDONLY...
+#include <filebuf.h>
 
-#ifndef _bufsiz
-#ifdef masscomp
-#ifdef _UCB
-#define _bufsiz(p) 4096
-#endif
-#else
-#define _bufsiz(p) ((p)->_bufsiz)
-#endif
-#endif
+filebuf::filebuf() 
+     :streambuf(), fd(-1), opened(0) {}
 
-#ifdef VMS
-#include <assert.h>
-#define FPOINT (*(Fp->fp))
-#undef _bufsiz
-#define _bufsiz(p) BUFSIZ	// we do not have this available
-#else
-#define FPOINT Fp->fp 
-#endif
+filebuf::filebuf(int newfd) 
+     : streambuf(), fd(newfd), opened(1) {}
 
-void Filebuf::init_streambuf_ptrs()
+filebuf::filebuf(int newfd, char* buf, int buflen)
+     : streambuf(buf, buflen), fd(newfd), opened(1) {}
+
+int filebuf::is_open()
 {
-  if (Fp->fp == 0 || FPOINT->_cnt == 0)
-  {
-    base = gptr = pptr = eptr = 0; // let first over/under flow deal with it
-  }
-  else
-  {
-#ifdef VMS
-    base = new char[BUFSIZ];
-    alloc = 1;
-#else
-    base = FPOINT->_base;
-#endif
-    eptr = base - 1 + _bufsiz(Fp->fp);
-    pptr = gptr = base;
-  }
+  return opened;
 }
 
-
-int Filebuf::is_open()
+int filebuf::close()
 {
-  return (Fp != 0 && Fp->is_open());
-}
-
-
-streambuf* Filebuf::open(const char* name, io_mode m, access_mode a)
-{
-  if (Fp == 0)
-    Fp = new File(name, m, a);
-  else
-    Fp->open(name, m, a);
-#ifndef VMS
-  if (base != 0) Fp->setbuf(eptr-base+1, base);
-#endif
-  init_streambuf_ptrs();
-  return this;
-}
-
-streambuf* Filebuf::open(const char* name, const char* m)
-{
-  if (Fp == 0)
-    Fp = new File(name, m);
-  else
-    Fp->open(name, m);
-#ifndef VMS
-  if (base != 0) Fp->setbuf(eptr-base+1, base);
-#endif
-  init_streambuf_ptrs();
-  return this;
-}
-
-streambuf*  Filebuf::open(const char* name, open_mode m)
-{
-  switch(m)
-  {
-  case input: return open(name, "r"); 
-  case output: return open(name, "w");
-  case append: return open(name, "a");
-  }
-}
-
-streambuf* Filebuf::open(int filedesc, io_mode m)
-{
-  if (Fp == 0)
-    Fp = new File(filedesc, m);
-  else
-    Fp->open(filedesc, m);
-#ifndef VMS
-  if (base != 0) Fp->setbuf(eptr-base+1, base);
-#endif
-  init_streambuf_ptrs();
-  return this;
-}
-
-streambuf* Filebuf::open(FILE* fileptr)
-{
-  if (Fp == 0)
-    Fp = new File(fileptr);
-  else
-    Fp->open(fileptr);
-#ifndef VMS
-  if (base != 0) Fp->setbuf(eptr-base+1, base);
-#endif
-  init_streambuf_ptrs();
-  return this;
-}
-
-Filebuf::Filebuf() : streambuf(), Fp(0) {}
-
-Filebuf::Filebuf(const char* filename, io_mode m, access_mode a)
-     : streambuf()
-{
-  Fp = new File(filename, m, a);
-  init_streambuf_ptrs();
-}
-
-Filebuf::Filebuf(const char* filename, const char* m)
-     : streambuf()
-{
-  Fp = new File(filename, m);
-  init_streambuf_ptrs();
-}
-
-Filebuf::Filebuf(int filedesc, io_mode m)
-     : streambuf()
-{
-  Fp = new File(filedesc, m);
-  init_streambuf_ptrs();
-}
-
-Filebuf::Filebuf(FILE* fileptr)
-     : streambuf()
-{
-  Fp = new File(fileptr);
-  init_streambuf_ptrs();
-}
-
-int Filebuf::close()
-{
-  int was = Fp->is_open();
-  if (was) { overflow(); Fp->close(); }
-#ifdef VMS
-  if (was) {
-	if(alloc && (base != 0)) {delete base; base=0; alloc = 0;};
-	gptr=0;};
-#endif
+  int was = opened;
+  if (was) ::close(fd);
+  opened = 0;
   return was;
 }
 
-
-Filebuf::~Filebuf()
+streambuf* filebuf::open(const char* name, open_mode m)
 {
-  if (Fp != 0)
+  if (opened) return 0;
+  int mode = -1; // any illegal value
+  switch (m)
   {
-    close();
-    delete Fp;
+  case input: mode = O_RDONLY; 
+              break;
+  case output: mode = O_WRONLY | O_CREAT | O_TRUNC;
+              break;
+  case append: mode = O_APPEND | O_CREAT | O_WRONLY;
+              break;
   }
-}
-
-#ifdef VMS
-/*
-	VMS implementation notes:
- The underflow routine works fine as long as there is no mixing of input and
- output for the same stream.  If this were to happen, then great confusion
- would occur, since we maintain a seperate buffer for the case of output.
- the stream classes do not allow this as they are currently written, so for
- now we are OK.  VMS does not handle buffered output in quite the same way
- that UNIX does, so a seperate buffer is allocated, and used by the program.
- when it comes time to flush it we call write(...) to empty it.  The flush
- function under VMS does not flush the buffer unless it is full, and whatsmore
- the _iobuf is not 14 bytes long as one might suspect from the structure def,
- but at *least* 66 bytes long.  Some of these hidden structure elements need
- to be set for the output to work properly, and it seemed to be poor
- programming practice to fool with them
-*/
-#endif
-/*
-  The underflow and overflow methods sync the streambuf with the _iobuf
-  ptrs on the way in and out of the read. I believe that this is
-  done in a portable way.
-*/
-int Filebuf::underflow()
-{
-  int ch;
-  if (Fp == 0) return EOF;
-  if (gptr == 0) // stdio _iobuf ptrs not initialized until after 1st read
+  fd = ::open(name, mode, 0666);
+  if (opened = (fd >= 0))
   {
-#ifdef VMS
-	assert(alloc==0);
-#endif
-    ch = Fp->fill();
-    base = FPOINT->_base;
-    eptr = base - 1 + _bufsiz(Fp->fp);
-  }
-  else
-  {
-    FPOINT->_ptr = gptr;
-    FPOINT->_cnt = eptr - gptr + 1;
-    ch = Fp->fill();
-  }
-  gptr = base;
-  *gptr = ch;
-  if (ch == EOF)
-    pptr = base;
-  else
-    pptr = base + FPOINT->_cnt + 1;
-  if (Fp->good())
-    return ch;
-  else
-  {
-    Fp->clear();
-    return EOF;
-  }
-}
-
-int Filebuf::overflow(int ch)
-{
-  if (Fp == 0) return EOF;
-  if (FPOINT->_flag & _IONBF)  // handle unbuffered IO specially
-  {
-    if (pptr == 0) // 1st write
-    {
-      if (ch == EOF)
-        return 0;
-      else
-      {
-        Fp->flush(ch);
-      }
-    }
-    else
-    {
-      if (ch == EOF)
-        Fp->flush();		// Probably not necessary
-      else
-        Fp->flush(ch);
-    }
-  }
-  else
-  {
-    if (pptr == 0) // 1st write
-    {
-      if (ch == EOF)
-        return 0;
-      else
-      {
-        Fp->flush(ch);
-#ifdef VMS
-        base = new char[BUFSIZ];
-        alloc = 1;
-#else
-        base = FPOINT->_base;
-#endif
-        eptr = base - 1 + _bufsiz(Fp->fp);
-        gptr = base;
-      }
-    }
-    else
-    {
-      if (ch != EOF) *pptr++ = ch;
-#ifdef VMS
-      if(gptr==base) write(FPOINT->_file,base,pptr-base);
-#else
-      FPOINT->_ptr = pptr;
-      FPOINT->_cnt = eptr - pptr + 1;
-#endif
-      Fp->flush();
-    }
-#ifdef VMS
-    pptr = base;
-#else
-    pptr = FPOINT->_ptr;
-#endif
-  }
-  if (Fp->fail() || Fp->bad())
-  {
-    Fp->clear(); // this allows recovery from ostream level
-    return EOF;
+    allocate();
+    return this;
   }
   else
     return 0;
 }
 
-const char* Filebuf::name()
+
+streambuf*  filebuf::open(const char* filename, io_mode m, access_mode a)
 {
-  return Fp->name();
+  return 0;
 }
 
-streambuf* Filebuf::setbuf(char* buf, int buflen, int preload)
+streambuf* filebuf::open(const char* filename, const char* m)
 {
-  if (preload != 0) return 0; // cannot preload, sorry
-  if (Fp != 0) Fp = new File;
-  Fp->setbuf(buflen, buf);
-  init_streambuf_ptrs();
-  return (Fp->good())? this : 0;
+  return 0;
 }
 
-void Filebuf::error()
+streambuf*  filebuf::open(int  filedesc, io_mode m)
 {
-  Fp->error();
+  return 0;
+}
+
+streambuf*  filebuf::open(FILE* fileptr)
+{
+  return 0;
+}
+
+int filebuf::underflow()
+{
+  if (!opened) return EOF;
+  if (base == 0) allocate();
+  int nwanted = eptr - base + 1;
+  int nread = ::read(fd, base, nwanted);
+  if (nread >= 0)
+  {
+    gptr = base;
+    pptr = base + nread;
+  }
+  return (nread <= 0)? EOF : int(*gptr);
+}
+
+int filebuf::overflow(int ch)
+{
+  if (!opened) return EOF;
+  if (base == 0) allocate();
+  if (ch != EOF)             // overflow *must* be called before really full
+    *pptr++ = (char)(ch);
+
+  // loop, in case write can't handle full request
+  // From: Rene' Seindal <seindal@diku.dk>
+
+  int w, n, t;
+  for (w = t = 0, n = pptr - base; n > 0; n -= w, t += w) 
+  {
+    if ((w = ::write(fd, base + t, n)) < 0)
+      break;
+  }
+ 
+  pptr = base;
+  return (n == 0 && w >= 0)? 0 : EOF;
+}
+
+filebuf::~filebuf()
+{
+  close();
 }
--- contrib/libg++-2.3/libg++/old-stream/Filebuf.h
+++ contrib/libg++-2.3/libg++/old-stream/Filebuf.h
@@ -1,61 +1,60 @@
 // This may look like C code, but it is really -*- C++ -*-
 /* 
-Copyright (C) 1988, 1992 Free Software Foundation
+Copyright (C) 1988 Free Software Foundation
     written by Doug Lea (dl@rocky.oswego.edu)
 
-This file is part of the GNU C++ Library.  This library is free
-software; you can redistribute it and/or modify it under the terms of
-the GNU Library General Public License as published by the Free
-Software Foundation; either version 2 of the License, or (at your
-option) any later version.  This library is distributed in the hope
-that it will be useful, but WITHOUT ANY WARRANTY; without even the
-implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the GNU Library General Public License for more details.
-You should have received a copy of the GNU Library General Public
-License along with this library; if not, write to the Free Software
-Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+This file is part of GNU CC.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY.  No author or distributor
+accepts responsibility to anyone for the consequences of using it
+or for whether it serves any particular purpose or works at all,
+unless he says so in writing.  Refer to the GNU CC General Public
+License for full details.
+
+Everyone is granted permission to copy, modify and redistribute
+GNU CC, but only under the conditions described in the
+GNU CC General Public License.   A copy of this license is
+supposed to have been given to you along with GNU CC so you
+can know your rights and responsibilities.  It should be in a
+file named COPYING.  Among other things, the copyright notice
+and this notice must be preserved on all copies.  
 */
 
-#ifndef _Filebuf_h
+#ifndef _filebuf_h
 #ifdef __GNUG__
+#if (__GNUG__ == 1)
+#pragma once
+#endif
 #pragma interface
 #endif
-#define _Filebuf_h 1
+#define _filebuf_h 1
 
-#include <File.h>
 #include <streambuf.h>
+#include <stdio.h>
 
-class Filebuf: public streambuf // libg++ File version
+class filebuf: public streambuf
 {
 public:
-  File*       Fp;
-
-  void        init_streambuf_ptrs();
+  int         fd;
+  char        opened;
 
   int         overflow(int c = EOF);
   int         underflow();
 
-              Filebuf();
-              Filebuf(const char* filename, io_mode m, access_mode a);
-              Filebuf(const char* filename, const char* m);   
-              Filebuf(int filedesc, io_mode m);
-              Filebuf(FILE* fileptr);
-  
-             ~Filebuf();
+              filebuf();
+              filebuf(int newfd);
+              filebuf(int newfd, char* buf, int buflen);
 
-  const char* name();
-  streambuf*  setbuf(char* buf, int buflen, int preloaded_count = 0);
+             ~filebuf();
 
   streambuf*  open(const char* name, open_mode m);
   streambuf*  open(const char* filename, io_mode m, access_mode a);
   streambuf*  open(const char* filename, const char* m);
   streambuf*  open(int  filedesc, io_mode m);
   streambuf*  open(FILE* fileptr);
-
   int         is_open();
   int         close();
-
-  void        error();
 };
 
 
